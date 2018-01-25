ee4dca5fef795c50b113b66fbdd9dc027083cc09
HaiwenZhang <haiwensummer@gmail.com>
2018-01-25 12:26:40 +0800

    React Router setup with couple of pages

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,3 +1,12 @@
 module.exports = {
-    "extends": "airbnb"
+    "extends": "airbnb",
+    "globals": {
+      "window": true,
+      "document": true
+    },
+    "rules": {
+      "react/jsx-filename-extension": [1, { "extensions": [".js", ".jsx"] }],
+      "jsx-a11y/href-no-hash": "off",
+      "jsx-a11y/anchor-is-valid": ["warn", { "aspects": ["invalidHref"] }]
+    }
 };
---
+++ frontend/src/components/pages/LoginPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const LoginPage = () => (
+  <di>
+    <h1>Login Page</h1>
+  </di>
+);
+
+export default LoginPage;
